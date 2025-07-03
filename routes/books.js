410e3620ad5cb501a5e6a95c7bfd64d36319d3de
kateemillerhd <kateemillerhd@gmail.com>
2025-07-03 00:43:31 +0000

    updated routes/book.js for trade functionality

--- routes/books.js
+++ routes/books.js
@@ -4,5 +4,7 @@ const bookController = require("../controllers/bookController");
 
 router.get("/", bookController.getAllBooks);
 router.post("/", bookController.addBook);
+router.post('/:id/request', bookController.proposeTrade);
+router.post('/:id/accept', bookController.acceptTrade);
 
 module.exports = router;
